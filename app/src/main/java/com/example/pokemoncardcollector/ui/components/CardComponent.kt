8b46dfefe2e84289a91dcfc602c36a55d2ecf3d8
Michael Hung Ling Jing <lj.michaelhung@gmail.com>
2025-05-18 15:40:22 +0800

    Update Card Component with mock data

--- app/src/main/java/com/example/pokemoncardcollector/ui/components/CardComponent.kt
+++ app/src/main/java/com/example/pokemoncardcollector/ui/components/CardComponent.kt
@@ -1,4 +1,68 @@
 package com.example.pokemoncardcollector.ui.components
 
-class CardComponent {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import com.example.pokemoncardcollector.domain.model.Card
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import com.example.pokemoncardcollector.domain.model.CardImage
+import com.example.pokemoncardcollector.domain.model.CardSet
+import com.example.pokemoncardcollector.domain.model.CardSetImage
+
+@Composable
+fun CardComponent(
+    modifier: Modifier = Modifier,
+    cardItem: Card,
+) {
+    Column {
+
+        AsyncImage(
+            model = cardItem.images.large,
+            contentDescription = "${cardItem.name}",
+            modifier = modifier
+        )
+
+        Column {
+
+
+        }
+
+    }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun CardComponentPreview() {
+    CardComponent(
+        modifier = Modifier,
+        cardItem = mockCard
+    )
+}
+
+private val mockCard = Card(
+    id = "xy7-54",
+    name = "Gardevoir EX",
+    supertype = "Pokémon",
+    subtypes = listOf("EX"),
+    types = listOf("Fairy"),
+    images = CardImage(
+        small = "https://images.pokemontcg.io/xy7/54.png",
+        large = "https://images.pokemontcg.io/xy7/54_hires.png"
+    ),
+    set = CardSet(
+        id = "xy7",
+        name = "Ancient Origins",
+        series = "XY",
+        printedTotal = 98,
+        images = CardSetImage(
+            symbol = "https://images.pokemontcg.io/xy7/symbol.png",
+            logo = "https://images.pokemontcg.io/xy7/logo.png"
+        )
+    ),
+    rarity = "Rare Holo EX",
+    number = "54"
+)
+
+
